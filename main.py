4637c1490da97ca17f7344871a44b215057c6078
Khodatskaa <khodatskaalina@gmail.com>
2023-09-19 20:32:43 +0300

    Ex 3 was done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-x = int(input(': '))
+x = int(input (': '))
 
-if:
-    print()
-elif:
-    print()
-else:
-    print()
+if x > 0:
+    print('Number is positive')
+elif x < 0:
+    print('Number is negative')
+elif x == 0:
+    print ('Number is equal to zero')
