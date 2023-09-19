add72ebb81345693dfab1f335b458f3b4756e833
Khodatskaa <khodatskaalina@gmail.com>
2023-09-19 20:26:54 +0300

    Ex 1 was done

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-x = int(input(': '))
+print('1. Monday')
+print ('2. Tuesday')
+print ('3. Wednesday')
+print ('4. Thursday')
+print ('5. Friday')
+print ('6. Saturday')
+print ('7. Sunday')
+choice = input('paste a number of operation(1/2/3/4/5/6/7): ')
 
-if:
-    print()
-elif:
-    print()
+if choice == "1":
+    print ('Monday')
+elif choice == "2":
+    print ('Tuesday')
+elif choice == "3":
+    print ('Wednesday')
+elif choice == "4":
+    print ('Thursday')
+elif choice == "5":
+    print('Friday')
+elif choice == "6":
+    print('Saturday')
+elif choice == "7":
+    print( 'Sunday')
 else:
-    print()
+    print( 'Unknown operation number')
