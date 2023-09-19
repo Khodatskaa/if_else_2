0d592fda92d2eaddde539d70c77c20a29e4ed14f
Khodatskaa <khodatskaalina@gmail.com>
2023-09-19 20:16:20 +0300

    Ex 5 was done

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 x = int(input(': '))
 
-if:
-    print()
-elif:
-    print()
+if 0 <= x <= 6:
+    print ('Good night')
+elif 7 <= x <= 13:
+    print('Good morning')
+elif 14 <= x <= 17:
+    print ('Good evening')
 else:
-    print()
+    print ('Unknown operation number')
