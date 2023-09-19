981f406b961761b0dd359abb9d56ce4125ceb4c2
Khodatskaa <khodatskaalina@gmail.com>
2023-09-19 20:30:15 +0300

    Ex 2 was done

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-x = int(input(': '))
+print('1. January')
+print('2. February')
+print('3. March')
+print('4. April')
+print('5. May')
+print('6. June')
+print('7. July')
+print('8. August')
+print('9. September')
+print('10. October')
+print('11. November')
+print('12. December')
+choice = input('paste a number of operation(1/2/3/4/5/6/7/8/9/10/11/12): ')
 
-if:
-    print()
-elif:
-    print()
+if choice == "1":
+    print ('January')
+elif choice == "2":
+    print ('February')
+elif choice == "3":
+    print ('March')
+elif choice == "4":
+    print ('April')
+elif choice =="5":
+    print ('May')
+elif choice == "6":
+    print ('June')
+elif choice == "7":
+    print ('July')
+elif choice == "8":
+    print ('August')
+elif choice == "9":
+    print ('September')
+elif choice == "10":
+    print('October')
+elif choice == "11":
+    print ('November')
+elif choice == "12":
+    print ('December')
 else:
-    print()
+    print('Unknown operation number')
