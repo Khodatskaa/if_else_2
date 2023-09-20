36be6506440956bd0add8177f2c3fe405d928419
Khodatskaa <khodatskaalina@gmail.com>
2023-09-20 22:09:42 +0300

    Ex 2 was done

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-x = int(input(': '))
+import math
 
-if:
-    print()
-elif:
-    print()
+d = float(input('Enter diameter: '))
+
+print('Choose an operation')
+print('1. Circle perimeter')
+print('2. Circle area')
+choice = input('Enter operation number(1/2): ')
+
+if choice == "1":
+    r = d / 2
+    perimeter = 2 * math.pi * r
+    print(perimeter)
+elif choice == "2":
+    r = d / 2
+    area = math.pi * r * r
+    print(area)
 else:
-    print()
+    print("Unknown operation number")
